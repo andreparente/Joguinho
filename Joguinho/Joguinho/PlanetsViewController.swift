adc132bee9fd012e8260399d7d375cad3e45b99f
Felipe Viberti <felipegus8@yahoo.com.br>
2016-11-09 16:36:31 -0200

    Mais storyboard

---
+++ Joguinho/Joguinho/PlanetsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PlanetsViewController.swift
+//  Joguinho
+//
+//  Created by Felipe Viberti on 09/11/16.
+//  Copyright © 2016 Andre Machado Parente. All rights reserved.
+//
+
+import UIKit
+
+class PlanetsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
