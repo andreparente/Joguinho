429041eeb587182722bf63f86908becbf534ccb4
Felipe Viberti <felipegus8@yahoo.com.br>
2016-11-19 15:21:30 -0200

    Alguma coisa

---
+++ Joguinho/Joguinho/InfoPlanetScene.swift
@@ -0,0 +1,14 @@
+//
+//  InfoPlanetScene.swift
+//  Joguinho
+//
+//  Created by Felipe Viberti on 19/11/16.
+//  Copyright © 2016 Andre Machado Parente. All rights reserved.
+//
+
+import SpriteKit
+
+class InfoPlanetScene: SKScene
+{
+      
+}
