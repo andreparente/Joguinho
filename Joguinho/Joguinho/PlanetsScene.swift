c81878d2ba6182f97dcd753b4fa652937f4124bd
Felipe Viberti <felipegus8@yahoo.com.br>
2016-11-24 22:03:43 -0200

    Build 2

--- Joguinho/Joguinho/PlanetsScene.swift
+++ Joguinho/Joguinho/PlanetsScene.swift
@@ -151,7 +151,6 @@ class PlanetsScene: SKScene{
                 print("tapped Neptune!")
                 
                 let planet = Planet(name: PlanetName.Neptune, gravity: 1.15, type: PlanetType.gaseous,index:PlanetIndex.Neptune)
-                let transition = SKTransition.reveal(with: SKTransitionDirection.up, duration: 1.0)
                 let transition2 = SKTransition.doorway(withDuration: 1.0)
                 let nextScene = SelectedPlanetScene(size: self.size)
                 nextScene.scaleMode = SKSceneScaleMode.aspectFill
