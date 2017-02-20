97f80c4ba75656d63f0f03b55f88fd5de530a721
Felipe Viberti <felipegus8@yahoo.com.br>
2017-02-20 17:17:51 -0300

    InGameStoreScene criada

---
+++ Joguinho/Joguinho/InGameStoreScene.swift
@@ -0,0 +1,26 @@
+//
+//  InGameStoreScene.swift
+//  Joguinho
+//
+//  Created by Felipe Viberti on 20/02/17.
+//  Copyright © 2017 Andre Machado Parente. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class InGameStoreScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        setupInitialScene()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    //MARK:Setup Functions
+    func setupInitialScene() {
+    }
+    
+}
